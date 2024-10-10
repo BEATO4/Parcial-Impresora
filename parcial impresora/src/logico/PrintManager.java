c9297e21f2e0734b9fc5cd8e91f6817842bd685d
BEATO4 <133258000+BEATO4@users.noreply.github.com>
2024-10-10 15:50:09 -0400

    Update PrintManager.java

--- parcial impresora/src/logico/PrintManager.java
+++ parcial impresora/src/logico/PrintManager.java
@@ -5,9 +5,12 @@ public class PrintManager {
         PrintService printService = new PrintService();
 
         // simulacion de envio de trabajos
-        printService.submitJob("Alice", "H");  // aalta prioridad
-        printService.submitJob("Bob", null);   // prioridad por defecto (Media)
-        printService.submitJob("Charlie", "L"); // baja prioridad
+        printService.submitJob("Jostin", "M");
+        printService.submitJob("Maria", "H");  // aalta prioridad
+        printService.submitJob("Jose", null);   // prioridad por defecto (Media)
+        printService.submitJob("Juan", "L"); // baja prioridad
+        printService.submitJob("Rafael", "M");
+        printService.submitJob("Javier", "H");
 
         // procesar los trabajos
         printService.processJobs();
