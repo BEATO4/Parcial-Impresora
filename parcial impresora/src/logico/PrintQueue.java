c2df71bd2a0e7e377c4a7e5fa4a451576ef706b3
BEATO4 <133258000+BEATO4@users.noreply.github.com>
2024-10-10 16:16:20 -0400

    Update PrintQueue.java

--- parcial impresora/src/logico/PrintQueue.java
+++ parcial impresora/src/logico/PrintQueue.java
@@ -6,18 +6,18 @@ public class PrintQueue {
     private PriorityQueue<PrintJob> queue;
 
     public PrintQueue() {
-        // La cola utiliza el metodo comparciono de PrintJob para ordenar los trabajos.
+        // La cola utiliza el metodo comparcion de PrintJob para ordenar los trabajos.
         this.queue = new PriorityQueue<>();
     }
 
-    // agregar un trabajo de impresion a la cola
+    // agregar un trabajo de impresion a cola
     public void addJob(PrintJob job) {
         queue.offer(job);
     }
 
     // Obtener y eliminar el siguiente trabajo en la cola
     public PrintJob getNextJob() {
-        return queue.poll(); // retira y devuelve el trabajo con mayor prioridad
+        return queue.poll(); // quita y devuelve el trabajo con mayor prioridad
     }
 
     // verificar si la cola esta vacia
